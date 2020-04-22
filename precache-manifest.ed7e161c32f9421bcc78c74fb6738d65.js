c129d1fcbf67d7611fcc975046e439229421c7e3
Firas Khlil <fkhlil@mail.arsd.dc>
2020-04-22 15:00:13 +0200

    Updates

---
+++ precache-manifest.ed7e161c32f9421bcc78c74fb6738d65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874125a93e6a9b2350ea8f1d76c0e5fd",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "5e4a406407c25d993ceb",
+    "url": "/contact-manager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7cd9433e0707800d9583",
+    "url": "/contact-manager/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "5e4a406407c25d993ceb",
+    "url": "/contact-manager/static/js/2.cd25d434.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact-manager/static/js/2.cd25d434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cd9433e0707800d9583",
+    "url": "/contact-manager/static/js/main.a64539ef.chunk.js"
+  },
+  {
+    "revision": "95764f8d3d158ff6711e",
+    "url": "/contact-manager/static/js/runtime-main.9db3588a.js"
+  }
+]);
