f93d2492a04e3bc6110f10b64e7c155c8bac92d4
Firas Khlil <fkhlil@mail.arsd.dc>
2020-04-22 14:54:12 +0200

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='display-4'>About Contact Manager App</h1>
+      <p className='lead'>Simple Application to manage contacts</p>
+      <p className='text-secondary'>Version 1.00</p>
+    </div>
+  );
+};
+
+export default About;
